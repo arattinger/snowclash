a6c2733821cca13fa7f916bc9958ece42bc0b9b1
Aleksandar Kojic <a.kojic@live.com>
2016-01-07 19:55:51 +0100

    Added .gitignore file, project prepared to be used over GIT (Edit->Project Settings->[Asset Serialization and Version Control options])

---
+++ Assets/Scripts/Player_NetworkSetup.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Player_NetworkSetup : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if (isLocalPlayer)
+        {
+            GetComponent<Player>().enabled = true;
+        }
+	}
+	
+	
+}
---
+++ Assets/Scripts/Zombie.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Zombie : MonoBehaviour {
+    public Transform victim;
+    NavMeshAgent navComponent;
+
+	// Use this for initialization
+	void Start () {
+        navComponent = GetComponent<NavMeshAgent>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                navComponent.SetDestination(hit.point);
+            }
+            
+        }
+        
+	}
+}
