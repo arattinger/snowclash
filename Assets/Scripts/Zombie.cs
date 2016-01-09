c4adca4b932603cd6da134b09e01d162cc19ac74
Aleksandar Kojic <a.kojic@live.com>
2016-01-09 18:51:53 +0100

    Animation created

--- Assets/Scripts/Zombie.cs
+++ Assets/Scripts/Zombie.cs
@@ -2,12 +2,13 @@
 using System.Collections;
 
 public class Zombie : MonoBehaviour {
-    NavMeshAgent navComponent;
+    NavMeshAgent navAgent;
+    public Animator animator;
 
 	// Use this for initialization
 	void Start () {
-        navComponent = GetComponent<NavMeshAgent>();
-        navComponent.updateRotation = false;
+        navAgent = GetComponent<NavMeshAgent>();
+        navAgent.updateRotation = false;
     }
 	
 	// Update is called once per frame
@@ -16,12 +17,41 @@ public class Zombie : MonoBehaviour {
         {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
             if (Physics.Raycast(ray, out hit))
             {
-                navComponent.SetDestination(hit.point);
+                Debug.Log("tag:" + hit.collider.tag);
+                if (hit.collider.tag == "Ground" || hit.collider.tag == "Obstacle")
+                {
+                    navAgent.SetDestination(hit.point);
+                }
             }
-            
         }
-        
+
+        UpdateAnimation();
 	}
+
+    void UpdateAnimation()
+    {
+        if (navAgent.velocity.x == 0)
+        {
+            animator.SetInteger("Direction", 0);
+        }
+        else if (navAgent.velocity.z > 0.2f)
+        {
+            animator.SetInteger("Direction", 8);
+        }
+        else if (navAgent.velocity.z < -0.2f)
+        {
+            animator.SetInteger("Direction", 2);
+        }
+        else if (navAgent.velocity.x > 0.2f)
+        {
+            animator.SetInteger("Direction", 6);
+        }
+        else if (navAgent.velocity.x < -0.2f)
+        {
+            animator.SetInteger("Direction", 4);
+        }
+    }
 }
