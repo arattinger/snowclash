b229a7730f6982cacea8d5add3a5a20f5b1cff40
ch0b0 <ch0b0@gmx.at>
2016-01-15 11:02:01 +0100

    game menu music toggle

---
+++ Assets/GameMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMenu : MonoBehaviour {
+
+	// Use this for initialization
+	public AudioClip clip;
+
+	void Start () 
+	{
+		if (clip == null)
+			return;
+
+		SoundManager.instance.PlaySingle (clip);
+	}
+}
---
+++ Assets/MusicScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MusicScript : MonoBehaviour {
+
+	public Toggle toggle;
+
+	public void MusicToggle()
+	{
+		Debug.Log ("ACTIVE: " + toggle.isOn);
+		if (!toggle.isOn)
+			SoundManager.instance.efxSource.Stop ();
+		else
+			SoundManager.instance.efxSource.Play ();
+	}
+}
