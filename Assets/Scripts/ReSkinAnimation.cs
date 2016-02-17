eb0d1fda7a5df6140f90dc14606fc60fae7f76eb
Andre Rattinger <andre.rattinger@student.TUGraz.at>
2016-02-17 18:54:33 +0100

    Added a reskined second enemy player

---
+++ Assets/Scripts/ReSkinAnimation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System;
+
+public class ReSkinAnimation : MonoBehaviour
+{
+
+    public string spriteSheetName;
+
+    void LateUpdate()
+    {
+
+        var subSprites = Resources.LoadAll<Sprite>("Characters/" + spriteSheetName);
+        Debug.Log(subSprites.Length);
+        foreach (var renderer in GetComponentsInChildren<SpriteRenderer>())
+        {
+            string spriteName = renderer.sprite.name;
+            Debug.Log("SpriteName: " + spriteName);
+            var newSprite = Array.Find(subSprites, item => item.name == spriteName);
+            Debug.Log("NewSprite: " + newSprite);
+            if (newSprite)
+                renderer.sprite = newSprite;
+        }
+    }
+}
+    
