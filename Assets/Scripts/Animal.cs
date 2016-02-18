2700b84b3263cbe5e86338e79535a4698f804d5b
Andre Rattinger <andre.rattinger@student.TUGraz.at>
2016-02-18 14:47:28 +0100

    Added a chicken :)

---
+++ Assets/Scripts/Animal.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Animal : AnimationUpdate {
+
+    public float moveCooldown = 2f;
+    float timer = 0;
+    public float dist;
+    Vector3 movePosition;
+
+    void Start () {
+        movePosition = this.transform.position;
+        navAgent = GetComponent<NavMeshAgent>();
+        navAgent.updateRotation = false;
+    }
+
+	void Update () {
+        timer += Time.deltaTime;
+
+        if(timer > moveCooldown)
+        {
+            float angle = Random.Range(0, 360);
+            var x = dist * Mathf.Cos(angle * Mathf.Deg2Rad);
+            var y = dist * Mathf.Sin(angle * Mathf.Deg2Rad);
+            movePosition = this.transform.position;
+            movePosition.x += x;
+            movePosition.z += y;
+            timer = 0;
+            StartCoroutine(ChangeDirection());
+            
+        }
+        UpdateAnimation();
+    }
+
+    IEnumerator ChangeDirection()
+    {
+        yield return new WaitForSeconds(0.5f);
+        navAgent.SetDestination(movePosition);
+    }
+}
---
+++ Assets/Scripts/AnimationUpdate.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationUpdate : MonoBehaviour {
+    public NavMeshAgent navAgent;
+    public Animator animator;
+
+    public void UpdateAnimation()
+    {
+        animator.SetFloat("Throw", 0.0f);
+        float horizontal = navAgent.desiredVelocity.normalized.x;
+        float vertical = navAgent.desiredVelocity.normalized.z;
+
+        if (Mathf.Abs(vertical) > Mathf.Abs(horizontal))
+        {
+            horizontal = 0;
+        }
+        else
+        {
+            vertical = 0;
+        }
+
+        //Debug.Log("horizonal: " + horizontal + " vertical: " + vertical);
+
+        Debug.Log(vertical + " && " + horizontal);
+        //Debug.Log(Vector3.Angle(new Vector3(0, navAgent.desiredVelocity.y, 0), navAgent.desiredVelocity));
+
+        if (vertical > 0)
+        {
+            //Debug.Log("Up");
+            animator.SetInteger("Direction", 2);
+            animator.SetFloat("Speed", 1f);
+        }
+        else if (vertical < 0)
+        {
+            //Debug.Log("Down");
+            animator.SetInteger("Direction", 0);
+            animator.SetFloat("Speed", 1f);
+        }
+        else if (horizontal < 0)
+        {
+            //Debug.Log("Left");
+            animator.SetInteger("Direction", 1);
+            animator.SetFloat("Speed", 1f);
+        }
+        else if (horizontal > 0)
+        {
+            //Debug.Log("Right");
+            animator.SetInteger("Direction", 3);
+            animator.SetFloat("Speed", 1f);
+        }
+        else
+        {
+            animator.SetFloat("Speed", 0.0f);
+        }
+    }
+}
